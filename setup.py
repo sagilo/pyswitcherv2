50b1772cb4750590f611364a13d4c75ad00c2c97
sagilo <5019344+sagilo@users.noreply.github.com>
2018-03-07 16:02:35 +0200

    Release 1.2.4
    Make necessary changes for uploading to PyPI
    
    Version 1.2.4

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'pyswitcherv2',
+  packages = ['pyswitcherv2'], 
+  version = '1.2.4',
+  description = 'Control Switcher V2 water heater via Python',
+  author = 'Sagi Lowenhardt',
+  author_email = 'sagilo@gmail.com',
+  url = 'https://github.com/sagilo/pyswitcherv2', 
+  download_url = 'https://github.com/sagilo/pyswitcherv2/archive/1.2.4.tar.gz',
+  include_package_data = True,
+  keywords = ['switcher', 'switcherv2'],
+  classifiers = [],
+)
+
