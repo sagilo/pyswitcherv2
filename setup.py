251a733a07b3a2530c742150cd1da0135d5d442a
sagilo <5019344+sagilo@users.noreply.github.com>
2018-03-07 18:31:10 +0200

    Version 1.2.3

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
   name = 'pyswitcherv2',
   packages = ['pyswitcherv2'], 
-  version = '1.2.2',
+  version = '1.2.3',
   description = 'Control Switcher V2 water heater via Python',
   author = 'Sagi Lowenhardt',
   author_email = 'sagilo@gmail.com',
   url = 'https://github.com/sagilo/pyswitcherv2', 
-  download_url = 'https://github.com/sagilo/pyswitcherv2/archive/1.2.2.tar.gz',
+  download_url = 'https://github.com/sagilo/pyswitcherv2/archive/1.2.3.tar.gz',
+  include_package_data = True,
   keywords = ['switcher', 'switcherv2'],
   classifiers = [],
 )
