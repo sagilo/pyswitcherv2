3eda02f1ed4d085b3ef47ab6b1b7c6646c33f078
Sagi Lowenhardt <5019344+sagilo@users.noreply.github.com>
2018-12-10 23:58:15 +0200

    Fix pcap parsing

--- pyswitcherv2/switcher.py
+++ pyswitcherv2/switcher.py
@@ -15,8 +15,9 @@ maxKnownFirmwareVersionToWorkWith = "71.0"
 
 g_def_credentials_filename = "credentials.json"
 
-def unpack(bytes):
-    size = len(bytes)
+
+def unpack(num_bytes):
+    size = len(num_bytes)
     fmt = '<'
     if size == 1:
         fmt += 'b'
@@ -26,7 +27,8 @@ def unpack(bytes):
         fmt += 'I'
     else:
         assert False, "Unexpected size: %d" % size
-    return struct.unpack(fmt, bytes)[0]
+    return struct.unpack(fmt, num_bytes)[0]
+
 
 def pack(integral, size):
     fmt = '<'
@@ -40,23 +42,28 @@ def pack(integral, size):
         assert False, "Unexpected size: %d" % size
     return struct.pack(fmt, integral)
 
+
 def get_command_from_header(header):
     return unpack(header[6:8])
+
 
 class ValidationError(Exception):
     def __init__(self, message):
         super(ValidationError, self).__init__(message)
 
+
 class ServerClosed(Exception):
     def __init__(self, message):
         super(ServerClosed, self).__init__(message)
+
 
 class OutOfReach(Exception):
     def __init__(self, message):
         super(OutOfReach, self).__init__(message)
 
-class Credentials():
-    def __init__(self, phone_id, device_id, device_pass, switcher_ip = None):
+
+class Credentials:
+    def __init__(self, phone_id, device_id, device_pass, switcher_ip=None):
         self._phone_id = phone_id
         self._device_id = device_id
         self._device_pass = device_pass
@@ -96,13 +103,14 @@ class Credentials():
     def switcher_ip(self):
         return self._switcher_ip if self._switcher_ip else "1.1.1.1"
 
-class Switcher():
+
+class Switcher:
     __SOCKET_PORT = 9957
     __SOCKET_RECEIVE_BYTES = 1024
     __SOCKET_TIMEOUT_SEC = 15
     __MAX_RETRIES = 3
 
-    def __init__(self, credentials, debug = False):
+    def __init__(self, credentials, debug=False):
         self._credentials = credentials
         self._debug = debug
         self._socket = None
@@ -129,7 +137,7 @@ class Switcher():
             is_on = self.__send_phone_state(session)
             print("Device is: %s" % ("on" if is_on else "off"))
             return is_on
-        except ServerClosed as s:
+        except ServerClosed:
             wait_seconds = retry_num * 3
             if self._debug and retry_num < self.__MAX_RETRIES:
                 print("Server closed, retrying in %d seconds" % wait_seconds)
@@ -150,9 +158,9 @@ class Switcher():
             self.__open_socket()
             session = self.__send_local_sign_in()
             self.__send_phone_state(session)
-            self.__send_control(is_on , time_minutes, session)
+            self.__send_control(is_on, time_minutes, session)
             return 0
-        except ServerClosed as s:
+        except ServerClosed:
             wait_seconds = retry_num * 3
             if self._debug and retry_num < self.__MAX_RETRIES:
                 print("Server closed, retrying in %d seconds" % wait_seconds)
@@ -193,7 +201,7 @@ class Switcher():
 
         try:
             print("Opening and connecting socket")
-            self._socket = socket.create_connection((host, port), timeout = timeout)
+            self._socket = socket.create_connection((host, port), timeout=timeout)
             if self._debug:
                 print("Socket connected to %s:%d" % (self._credentials.switcher_ip(), self.__SOCKET_PORT))
             else:
@@ -205,7 +213,8 @@ class Switcher():
     # reques header utils
     ##########################################################################################
 
-    def __update_request_constants(self, header):
+    @staticmethod
+    def __update_request_constants(header):
         header[0:1] = pack(-2, 1)
         header[1:2] = pack(-16, 1)
         header[4:5] = pack(2, 1)
@@ -223,7 +232,7 @@ class Switcher():
         header[8:12] = pack(session, 4)
         header[12:14] = pack(serial, 2)
         header[14:15] = pack(dirty, 1)
-        #header[18:26] = 0 # 6 bytes did, not required
+        # header[18:26] = 0 # 6 bytes did, not required
         header[24:28] = pack(timestamp, 4)
 
     ##########################################################################################
@@ -231,7 +240,7 @@ class Switcher():
     ##########################################################################################
 
     def __update_local_sign_in_body(self, data):
-        data[40:42] = binascii.unhexlify("1c00") # version
+        data[40:42] = binascii.unhexlify("1c00")  # version
         data[42:46] = binascii.unhexlify(self._credentials.phone_id())
         data[46:50] = binascii.unhexlify(self._credentials.device_pass())
         data[76:78] = pack(0, 2)
@@ -300,7 +309,7 @@ class Switcher():
         minutes_on = unpack(response[93:97]) / 60
 
         if self._debug:
-            #print("Device name: %s" % response[40:64])
+            # print("Device name: %s" % response[40:64])
             print("Got phone state response, session: %d, on: %d, minutes to off: %d, minutes on: %d, response: \n\t%s\n" % (session, is_on, minutes_to_off, minutes_on, binascii.hexlify(response)))
         else:
             print("Got phone state response")
@@ -317,14 +326,14 @@ class Switcher():
         data[48:52] = binascii.unhexlify(self._credentials.device_pass())
 
         data[80:81] = pack(1, 1) 
-        data[81:83] = pack(6, 1) # TODO constant ? 
+        data[81:83] = pack(6, 1)  # TODO constant ?
         assert unpack(data[80:81]) == 1, "expected 1, got %d" % unpack(data[80:81])
         assert unpack(data[81:83]) == 6, "expected 6, got %d" % unpack(data[81:83])
         data[83:84] = pack(is_on, 1)
         assert unpack(data[84:85]) == 0, "expected 0, got %d" % unpack(data[84:85])
         data[85:89] = pack(time_min * 60, 4)
 
-    def __genereate_control_request(self, is_on, time_minutes, session):        
+    def __generate_control_request(self, is_on, time_minutes, session):
         CONTROL_COMMAND = 513
         CONTROL_REQ_LENGTH = 93
 
@@ -335,7 +344,7 @@ class Switcher():
         return self.__calc_crc(data)
         
     def __send_control(self, is_on, time_minutes, session):
-        request = self.__genereate_control_request(is_on, time_minutes, session)
+        request = self.__generate_control_request(is_on, time_minutes, session)
 
         if self._debug:
             print("Sending control request, is_on: %d, minutes: %d: \n\t%s" % (is_on, time_minutes, binascii.hexlify(request)))
@@ -349,12 +358,11 @@ class Switcher():
         else:
             print("Got control response")
 
-
     ##########################################################################################
     # crc
     ##########################################################################################
-
-    def __calc_crc(self, data, key = "00000000000000000000000000000000"): 
+    @staticmethod
+    def __calc_crc(data, key ="00000000000000000000000000000000"):
         crc = bytearray(struct.pack('>I', binascii.crc_hqx(data, 4129)))
         data = data + crc[3:4] + crc[2:3]
         crc = crc[3:4] + crc[2:3] + bytearray(key, 'utf8')
@@ -366,6 +374,7 @@ class Switcher():
 # commander functions
 ##########################################################################################
 
+
 def exit_with_error(msg):
     err_msg = "ERROR: %s" % msg
     print(err_msg)
@@ -375,10 +384,11 @@ def exit_with_error(msg):
 # parsing
 ##########################################################################################
 
+
 def extract_credentials_from_pcap(pcap_file, is_debug):
     try:
         from pcapfile import savefile
-    except ImportError as e:
+    except ImportError:
         raise Exception("Missing 'pypcapfile' package, please install (pip install pypcapfile)")
 
     if is_debug:
@@ -388,11 +398,11 @@ def extract_credentials_from_pcap(pcap_file, is_debug):
         capfile = savefile.load_savefile(file, layers=1, verbose=True)
         print("\n")
         for packet in capfile.packets:
-            packet = bytearray(packet.packet.payload)
-            if (len(packet) <= 40): # tcp header
+            packet = bytearray(binascii.unhexlify(packet.packet.payload))
+            if len(packet) <= 40:  # tcp header
                 continue
 
-            packet = packet[40:] # tcp header
+            packet = packet[40:]  # tcp header
 
             command = get_command_from_header(packet)
             if command != 513:
@@ -410,11 +420,12 @@ def extract_credentials_from_pcap(pcap_file, is_debug):
 
         exit_with_error("ERROR: Didn't find relevant ids in pcap file")
     except Exception as exception:
-        raise Exception("Error pasing pcap file: %s" % exception)
+        raise Exception("Error parsing pcap file (invalid pcap file?): %s" % exception)
 
 ##########################################################################################
 # credentials
 ##########################################################################################
+
 
 def is_hex(s):
     try:
@@ -422,6 +433,7 @@ def is_hex(s):
         return True
     except ValueError:
         return False
+
 
 def read_credentials(credentials_file):
     data = json.load(open(credentials_file))
@@ -435,19 +447,22 @@ def read_credentials(credentials_file):
     credentials.validate()
     return credentials
 
+
 def write_credentials(credentials, credentials_file):
-    data = {}
-    data["phone_id"] = credentials.phone_id()
-    data["device_id"] = credentials.device_id()
-    data["device_pass"] = credentials.device_pass()
-    data["ip"] = credentials.switcher_ip()
+    data = {
+        "phone_id": credentials.phone_id(),
+        "device_id": credentials.device_id(),
+        "device_pass": credentials.device_pass(),
+        "ip": credentials.switcher_ip()
+    }
 
     with open(credentials_file, 'w') as outfile:
-         json.dump(data, outfile)
+        json.dump(data, outfile)
 
 ##########################################################################################
 # modes
 ##########################################################################################
+
 
 def parse_pcap_file(pcap_file, credentials_file, is_debug):
     credentials = extract_credentials_from_pcap(pcap_file, is_debug)
@@ -459,6 +474,7 @@ def parse_pcap_file(pcap_file, credentials_file, is_debug):
 
     return 0
 
+
 def set_credentials(credentials, credentials_file):
     write_credentials(credentials, credentials_file)
     print("Updated credentials file successfully")
@@ -469,14 +485,15 @@ def set_credentials(credentials, credentials_file):
 # main
 ##########################################################################################
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Help me')
     mode_choices = ["on", "off", "get_state", "parse_pcap_file", "set_credentials"]
-    parser.add_argument('-m','--mode', dest='mode', choices=mode_choices, required=True)
-    parser.add_argument('-t','--time', dest='time_min', default=0, type=int, required=False)
-    parser.add_argument('-f','--file_path', dest='pcap_file', help="Pcap file to parse (requires pypcapfile package)", required=False)
-    parser.add_argument('-d','--debug', dest='debug', default=False, action='store_true', required=False)
-    parser.add_argument('-c','--credentials_file_path', dest='credentials_file', help='Path to credentials file if not next to script', required=False)
+    parser.add_argument('-m', '--mode', dest='mode', choices=mode_choices, required=True)
+    parser.add_argument('-t', '--time', dest='time_min', default=0, type=int, required=False)
+    parser.add_argument('-f', '--file_path', dest='pcap_file', help="Pcap file to parse (requires pypcapfile package)", required=False)
+    parser.add_argument('-d', '--debug', dest='debug', default=False, action='store_true', required=False)
+    parser.add_argument('-c', '--credentials_file_path', dest='credentials_file', help='Path to credentials file if not next to script', required=False)
     parser.add_argument('--device_id', dest='device_id', required=False, help="Device ID to set when using 'set_credentials' mode")
     parser.add_argument('--phone_id', dest='phone_id', required=False, help="Phone ID to set when using 'set_credentials' mode")
     parser.add_argument('--device_pass', dest='device_pass', required=False, help="Device pass to set when using 'set_credentials' mode")
@@ -486,7 +503,7 @@ def parse_args():
     mode = args.mode
     if mode == 'parse_pcap_file':
         pcap_file = args.pcap_file
-        if pcap_file == None:
+        if not pcap_file:
             exit_with_error("No file given for parsing (-f)")
         elif not os.path.isfile(pcap_file):
             exit_with_error("Can't find pcap file: '%s'" % pcap_file)
@@ -513,6 +530,7 @@ def parse_args():
 
     return args
 
+
 def run(args):
     mode = args.mode
 
@@ -532,17 +550,20 @@ def run(args):
     elif mode == 'on':
         return switcher.turn_on(args.time_min)
     elif mode == 'off':
-         return switcher.turn_off()
+        return switcher.turn_off()
 
     raise Exception("Unexpected mode (please report to developer): '%s'" % mode)
-    
+
+
 def get_timestamp():
     return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+
 
 def main():
     args = parse_args()
     print("%s | mode: %s" % (get_timestamp(), args.mode))
     sys.exit(run(args))
+
 
 if __name__ == '__main__': 
     main()
